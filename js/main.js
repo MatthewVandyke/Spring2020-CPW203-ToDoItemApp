2983fc85a4cb88c578fff5046aa34894ad3ce6c6
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-05-18 12:14:30 -0700

    added basic ts structure

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 var picker = datepicker("#due-date");
 picker.setMin(new Date());
+var ToDoItem = (function () {
+    function ToDoItem(title) {
+        this.title = title;
+    }
+    return ToDoItem;
+}());
+function isValid() {
+    return true;
+}
+function getToDoItem() {
+    var item = new ToDoItem("testing");
+    return item;
+}
+function displayToDoItem(item) {
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,2 +1,43 @@
+// @ts-ignore: Ignoring issue with js-datepicker lack of IntelliSense
 const picker = datepicker("#due-date");
 picker.setMin(new Date()); // Set to today's date
+
+class ToDoItem{
+    title:string;
+    dueDate:Date;
+    isCompleted:boolean;
+
+    constructor(title:string){
+        this.title = title;
+    }
+}
+/*
+let item = new ToDoItem("testing");
+item.dueDate = new Date(2020, 6, 1);
+item.isCompleted = false; 
+*/
+
+/**
+ * Check form data is valid
+ */
+function isValid():boolean{
+    return true;
+}
+
+/**
+ * Get all input off form and wrap in 
+ * a ToDoItem object
+ */
+function getToDoItem():ToDoItem{
+    let item = new ToDoItem("testing");
+    return item;
+}
+
+/**
+ * Diplay given ToDoItem on the web page
+ */
+function displayToDoItem(item:ToDoItem):void{
+
+}
+
+// Task: allow user to mark
